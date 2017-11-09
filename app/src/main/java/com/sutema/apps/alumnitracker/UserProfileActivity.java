541bb6f9ddb6ed7bd6820565faefec2aef1c3cd5
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-11-09 21:22:29 +0700

    render edit form dengan data dummy

--- app/src/main/java/com/sutema/apps/alumnitracker/UserProfileActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/UserProfileActivity.java
@@ -2,6 +2,7 @@ package com.sutema.apps.alumnitracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,34 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        User user = getUserProfile();
+
+        //Set user profile in Form
+        renderFormData(user);
+    }
+
+    User getUserProfile(){
+        User user = new User();
+        user.setID(9999);
+        user.setFullName("Dummy People");
+        user.setAddress("Jalan kehidupan");
+        user.setEmail("dummy@people.com");
+        user.setPhone("089999991234");
+
+        return user;
+    }
+
+    void renderFormData(User user){
+        EditText fullNameTxt = findViewById(R.id.editText);
+        EditText addressTxt = findViewById(R.id.editText3);
+        EditText emailTxt = findViewById(R.id.editText5);
+        EditText phoneTxt = findViewById(R.id.editText6);
+
+        fullNameTxt.setText(user.getFullName());
+        addressTxt.setText(user.getAddress());
+        emailTxt.setText(user.getEmail());
+        phoneTxt.setText(user.getPhone());
+
     }
 }
