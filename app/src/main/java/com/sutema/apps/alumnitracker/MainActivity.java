2d47d24b379604d7111e101ac6d836b938a7df21
Ulil Amri <ulilamri07@gmail.com>
2017-11-11 14:54:32 +0700

    no message

--- app/src/main/java/com/sutema/apps/alumnitracker/MainActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sutema.apps.alumnitracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         //test comment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Register(View view){
+        //Klik Daftar
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/sutema/apps/alumnitracker/SignupActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/SignupActivity.java
@@ -1,7 +1,12 @@
 package com.sutema.apps.alumnitracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void SubmitSignUp (View view){
+        Intent intent = new Intent(this, HomepageActivity.class);
+        startActivity(intent);
     }
 }
