563dac3efad42665b6ad52affc9d4e5787ac33d1
Fajar Lazuardi <fajarlazuardijr@gmail.com>
2017-12-06 23:54:56 +0700

    Penambahan UI untuk menampilkan hasil json api openweather dengan pencarian berdasarkan kota

--- app/src/main/java/com/sutema/apps/alumnitracker/JSONDemoActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/JSONDemoActivity.java
@@ -1,35 +1,70 @@
 package com.sutema.apps.alumnitracker;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class JSONDemoActivity extends AppCompatActivity {
+
+    EditText cityName;
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jsondemo);
 
-        DownloadTask task = new DownloadTask();
-        task.execute("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1");
+        cityName = (EditText)findViewById(R.id.cityName);
+        resultTextView = findViewById(R.id.resultTextView);
+
+    }
+
+    public void findWeather (View view) {
+
+
+        Log.i("City name", cityName.getText().toString());
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(cityName.getWindowToken(), 0);
+
+        try {
+
+            String encodedCityName = URLEncoder.encode(cityName.getText().toString(), "UTF-8");
+
+            DownloadTask task = new DownloadTask();
+            task.execute("http://api.openweathermap.org/data/2.5/weather?q="+ encodedCityName +"&APPID=96cb6d9e1f50e65f7872eb88839f8391");
+
+        } catch (UnsupportedEncodingException e) {
+
+            e.printStackTrace();
+
+            //Toast.makeText(getApplicationContext(), "Could not find the Weather", Toast.LENGTH_LONG);
+        }
 
     }
 
     public class DownloadTask extends AsyncTask <String, Void, String> {
-
 
         @Override
         protected String doInBackground(String... urls) {
@@ -56,10 +91,9 @@ public class JSONDemoActivity extends AppCompatActivity {
 
                 return result;
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+
+                //Toast.makeText(getApplicationContext(), "Could not find the Weather", Toast.LENGTH_LONG);
             }
 
             return null;
@@ -70,6 +104,9 @@ public class JSONDemoActivity extends AppCompatActivity {
             super.onPostExecute(result);
 
             try {
+
+                String message = "";
+
                 JSONObject jsonObject = new JSONObject(result);
                 String weatherInfo = jsonObject.getString("weather");
 
@@ -81,12 +118,32 @@ public class JSONDemoActivity extends AppCompatActivity {
 
                     JSONObject jsonPart = arr.getJSONObject(i);
 
-                    Log.i("main", jsonPart.getString("main"));
-                    Log.i("description", jsonPart.getString("description"));
+                    String main = "";
+                    String description = "";
+
+                    main = jsonPart.getString("main");
+                    description = jsonPart.getString("description");
+
+                    if (main != "" && description != "") {
+
+                        message += main + ": " + description +"\r\n";
+
+                    }
                 }
 
+                if (message != "") {
+
+                    resultTextView.setText(message);
+
+                } //else {
+
+                    //Toast.makeText(getApplicationContext(), "Could not find the Weather", Toast.LENGTH_LONG);
+                //}
+
             } catch (JSONException e) {
-                e.printStackTrace();
+
+                //Toast.makeText(getApplicationContext(), "Could not find the Weather", Toast.LENGTH_LONG);
+
             }
 
 
