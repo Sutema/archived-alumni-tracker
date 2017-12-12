744f1fcbc02007455812e12e83712ca6d1d9a6f7
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-12-13 05:32:15 +0700

    + UI Form Loker

---
+++ app/src/main/java/com/sutema/apps/alumnitracker/LokerFormActivity.java
@@ -0,0 +1,13 @@
+package com.sutema.apps.alumnitracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LokerFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loker_form);
+    }
+}
--- app/src/main/java/com/sutema/apps/alumnitracker/MyLokerActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/MyLokerActivity.java
@@ -1,9 +1,12 @@
 package com.sutema.apps.alumnitracker;
 
 import android.arch.persistence.room.Room;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.util.Date;
 
@@ -14,5 +17,18 @@ public class MyLokerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_loker);
 
+        FloatingActionButton createLokerBtn = findViewById(R.id.floatingActionButton);
+        createLokerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLokerForm();
+            }
+        });
+
+    }
+
+    public void openLokerForm(){
+        Intent intent = new Intent(this, LokerFormActivity.class);
+        startActivity(intent);
     }
 }
