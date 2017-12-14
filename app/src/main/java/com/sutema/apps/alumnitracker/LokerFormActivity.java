66642b53e28af47b72583235aeaf56b047de1932
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-12-14 23:21:20 +0700

    tambah Fetch Loker extends asyncTask

--- app/src/main/java/com/sutema/apps/alumnitracker/LokerFormActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/LokerFormActivity.java
@@ -9,5 +9,7 @@ public class LokerFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loker_form);
+
+
     }
 }
--- app/src/main/java/com/sutema/apps/alumnitracker/MyLokerActivity.java
+++ app/src/main/java/com/sutema/apps/alumnitracker/MyLokerActivity.java
@@ -1,23 +1,20 @@
 package com.sutema.apps.alumnitracker;
 
-import android.app.Activity;
-import android.arch.persistence.room.Room;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import java.util.Date;
-import java.util.List;
 
 public class MyLokerActivity extends AppCompatActivity {
 
     private TextView notifTxt;
     private ImageView emptyIcon;
+    private TextView progressTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +22,9 @@ public class MyLokerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_loker);
 
         notifTxt = findViewById(R.id.textView3);
+        progressTxt = findViewById(R.id.textView6);
 
+/*
         AppDatabase appDatabase = null;
         try {
             appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "sutema.db").allowMainThreadQueries().build();
@@ -44,6 +43,9 @@ public class MyLokerActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+*/
+        FetchLoker fetchLoker = new FetchLoker();
+        fetchLoker.execute();
 
         FloatingActionButton createLokerBtn = findViewById(R.id.floatingActionButton);
         createLokerBtn.setOnClickListener(new View.OnClickListener() {
@@ -59,4 +61,42 @@ public class MyLokerActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LokerFormActivity.class);
         startActivity(intent);
     }
+    public class FetchLoker extends AsyncTask<Void, Integer, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            notifTxt.setText("Pre Execute State");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            notifTxt.setText("Post Execute State");
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressTxt.setText(values[0]+"% completed...");
+
+
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            for (int i = 0;i < 1000; i++){
+                try{
+                    publishProgress(i*100/1000);
+                    Thread.sleep(10);
+                }catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+            }
+            return null;
+        }
+    }
 }
+
+
+
+
